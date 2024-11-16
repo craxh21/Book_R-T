2c3f873109d454af6555a9f4543a27f7a07245f9
craxh21 <mbcraxh21@gmail.com>
2024-11-16 22:33:31 +0530

    registration

--- app.py
+++ app.py
@@ -1,41 +1,55 @@
-from flask import Flask,render_template, request, session, redirect
+from flask import Flask,render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 import json
 
-local_server = True
+# local_server = True
 
 with open('config.json', 'r') as c:  
     params = json.load(c)["params"] 
 
 app = Flask(__name__)
+app.secret_key = 'the_random_string' 
 
-if(local_server):
-    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
-else:  #else to the production uri
-    app.config['SQLALCHEMY_DATABASE_URI'] = params['production_uri']
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/mydb'
+  
 db = SQLAlchemy(app)
 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80),  nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
+@app.route("/")
+def index():
+    return "Hello"
 
-# class Users(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80),  nullable=False)
-#     email = db.Column(db.String(20), nullable=False)
-#     password = db.Column(db.String(13),nullable=False)
-    
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    if(request.method == 'POST'):
+        username = request.form.get('uname')
+        email = request.form.get('uemail')
+        password = request.form.get('upass')
 
-@app.route('/<user>/<u2>')
-def home(user,u2):
-    return f"hello {user} and {u2}"
+        # making entry
+        entry = Users(username=username, email = email, password = password)
+        # adding the entry
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('login.html')
 
-@app.route('/dashboard', methods = ['GET', 'POST'])
+@app.route("/dashboard", methods = ['GET', 'POST'])
 def dashboard():
-    if('user' in session):
-        return render_template("login.html")
+    return "this is dashboard"
 
-    return render_template('login.html', params= params)
+# @app.route("/login", methods = ['GET', 'POST'])
+# def login():
+#     if(request.method == 'POST'):
+#         #handle request
+#         pass
+#     return render_template('login.html')
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
